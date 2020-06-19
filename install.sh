0418fbad5ad347ebf25a545889a0859a4ce05fd8
Nils Pukropp <nils.pukropp@outlook.de>
2020-06-19 11:36:40 +0200

    added bashrc

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+sudo mkdir ~/tmp
+cp -r * ~/tmp
+cd ~
 curl -sL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
-sudo rm -rf nodesource_setup.sh
 sudo apt-get install nodejs -y
 sudo apt-get install neovim -y
 sudo apt-get install openjdk-14-jdk -y
@@ -11,14 +13,11 @@ sudo apt-get install wget -y
 sudo apt-get update
 sudo sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-sudo cp -r .config/nvim/* ~/.config/nvim/
+sudo mkdir -p ~/.config/nvim/
+sudo cp -r tmp/.config/nvim/* ~/.config/nvim/
 wget https://mirror.netcologne.de/apache.org/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
 tar xzvf apache-maven-3.6.3-bin.tar.gz
-sudo rm -rf /opt/apache-maven-3.6.3
 sudo mv apache-maven-3.6.3 /opt/
-sudo rm -rf apache-master-3.6.3-bin.tar.gz
-sudo rm -rf apache-maven-3.6.3
-
 sudo apt install apt-transport-https curl
 
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
@@ -29,5 +28,10 @@ sudo apt update
 
 sudo apt install brave-browser -y
 
-sudo rm ~.bashrc
-sudo cp .bashrc ~
+sudo rm .bashrc
+sudo cp tmp/.bashrc ~
+sudo rm -rf apache-master-3.6.3-bin.tar.gz
+sudo rm -rf apache-maven-3.6.3
+sudo rm -rf /opt/apache-maven-3.6.3
+sudo rm -rf tmp
+
