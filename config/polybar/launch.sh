8c6f1e87dde256c5828b658e2e104d2adf4b5160
Nils Pukropp <nils@nirusu.codes>
2021-12-11 14:06:47 +0100

    updated

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -15,5 +15,5 @@ polybar --config=$DIR/config.ini -q workspaces2 &
 polybar --config=$DIR/config.ini -q systeminfo &
 polybar --config=$DIR/config.ini -q applications &
 polybar --config=$DIR/config.ini -q topinfo &
-polybar --config=$DIR/config.ini -q spotifycontrolbar &
-polybar --config=$DIR/config.ini -q tray &
+# polybar --config=$DIR/config.ini -q spotifycontrolbar &
+# polybar --config=$DIR/config.ini -q tray &
