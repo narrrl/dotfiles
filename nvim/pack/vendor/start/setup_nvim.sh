0244eef63dfe7f03cda3003f4fa4d76d52a7412b
Nils Pukropp <nils@narl.io>
2025-07-23 23:01:04 +0200

    added submodules

--- nvim/pack/vendor/start/setup_nvim.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -rf ~/.config/nvim 
-cp -r nvim ~/.config/
-git clone https://github.com/neovim/nvim-lspconfig ~/.config/nvim/pack/vendor/start/nvim-lspconfig
-git clone https://github.com/echasnovski/mini.nvim ~/.config/nvim/pack/vendor/start/mini.nvim
