fe2e9718503c8ccc2c83938a49d82feee1bb448e
Nils Pukropp <nils@narl.io>
2025-11-25 00:29:47 +0100

    updated some stuff

--- waybar/scripts/cpu_info.sh
+++ waybar/scripts/cpu_info.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 USAGE=$(mpstat 1 1 | awk '/Average:/ {print 100 - $12}')
-TEMP=$(cat /sys/class/hwmon/hwmon3/temp1_input)
+TEMP=$(cat /sys/class/hwmon/hwmon6/temp1_input)
 TEXT="$(printf "%.1f" $(echo "$USAGE"))% $(printf "%.1f" $(echo "scale=2; $TEMP"/1000 | bc -l))C"
 CPU=$(lscpu | grep 'Model name' | awk -F': ' '{print $2}' | sed 's/^[ \t]*//')
 # TOOLTIP=$(ps -eo %cpu,comm --sort=-%cpu | head -n 6 | sed '1d' | awk '{output = output sprintf("%.1f%%\t%s\\n", $1, $2)} END {printf "%s", output}')
